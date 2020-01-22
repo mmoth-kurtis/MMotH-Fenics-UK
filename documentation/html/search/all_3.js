177cde242a6f3eaad0a2c1d103811b36963131fd
ckma224@uky.edu <ckma224@g.uky.edu>
2020-01-22 14:43:16 -0500

    removed old doxygen files

--- documentation/html/search/all_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['left_5',['Left',['../classpm__test10_1_1_left.html',1,'pm_test10.Left'],['../classpm__test10__py3_1_1_left.html',1,'pm_test10_py3.Left']]],
-  ['lower_6',['Lower',['../classpm__test10_1_1_lower.html',1,'pm_test10.Lower'],['../classpm__test10__py3_1_1_lower.html',1,'pm_test10_py3.Lower']]]
-];
