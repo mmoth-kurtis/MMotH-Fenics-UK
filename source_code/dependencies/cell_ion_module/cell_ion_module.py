b84011eacea7e5c9237d049fa2c76552323f776a
ckma224@uky.edu <ckma224@g.uky.edu>
2020-02-03 14:36:55 -0500

    got cell ion module working (I think)

--- source_code/dependencies/cell_ion_module/cell_ion_module.py
+++ source_code/dependencies/cell_ion_module/cell_ion_module.py
@@ -1,5 +1,10 @@
+# So far these are only needed to test standalone
+import sys
+import numpy as np
 
-
+# Different path because not running in fenics yet
+#sys.path.append("home/fenics/shared/source_code/dependencies/cell_ion_module")
+import recode_dictionary
 
 ## Class for cell ion models
 #
@@ -9,4 +14,11 @@ class cell_ion_module():
     def __init__(self,params):
 
         # Specify model to be ran
-        model = params["model"]
+        temp = params["model"][0]
+
+        base_dir = "cell_ion_module."
+        model_name = base_dir + temp
+
+        # Import the model
+        self.model = __import__(model_name)
+        #help(self.model)
