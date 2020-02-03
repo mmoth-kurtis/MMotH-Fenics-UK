20503837cae5fdfd1a35514704893f5a205a7d08
ckma224@uky.edu <ckma224@g.uky.edu>
2020-02-03 15:26:35 -0500

    had to convert time

--- source_code/dependencies/cell_ion_module/three_state_calcium.py
+++ source_code/dependencies/cell_ion_module/three_state_calcium.py
@@ -7,6 +7,8 @@ def display_success():
 
 def calculate_concentrations(cycle,time):
 
+    # Time is passed in as ms, not seconds
+    t = time/1000
     # Don't plan on using this transient much, hard coding some stuff
     t_act = 0.08
     cardiac_period = .17
@@ -16,15 +18,15 @@ def calculate_concentrations(cycle,time):
     calcium_value = 0.0
     pCa = 7
 
-    if time <= t_act:
+    if t <= t_act:
         pCa = 7
         calcium_value = 10**(-1*pCa)
 
-    elif ((cardiac_period*cycle+t_act) < time) and (time < (t_p)):
-        pCa = (time - (cardiac_period*cycle+t_act))/0.02
+    elif ((cardiac_period*cycle+t_act) < t) and (t < (t_p)):
+        pCa = (t - (cardiac_period*cycle+t_act))/0.02
 
-    elif (time >= t_p):
-        pCa = 0.5*np.exp(-np.power((time - t_p)*fCa, fCa_2))
+    elif (t >= t_p):
+        pCa = 0.5*np.exp(-np.power((t - t_p)*fCa, fCa_2))
 
     calcium_value = (0.1 + 1000 * np.sin(3.14*pCa)) * 1E-9
 
